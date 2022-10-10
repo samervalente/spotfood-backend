9d79c313c0bf5fd2d48b70de2a63adeecb3185a0
samervalente <samervalente@gmail.com>
2022-10-10 10:59:45 -0300

    fix: adjust use cors express sintax

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,8 @@ import routes from "./routes/index"
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware"
 
 const app = express()
-app.use([cors(), express.json()])
+app.use(cors())
+app.use(express.json())
 
 app.use(routes)
 app.use(errorHandlerMiddleware)
