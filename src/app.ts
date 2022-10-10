1ed4502f1f9b7af65dead8b20f0db67c22b01102
samervalente <samervalente@gmail.com>
2022-10-10 11:40:54 -0300

    feat: allow multiple origins in cors policy

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,12 @@ import routes from "./routes/index"
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware"
 
 const app = express()
-app.options('*', cors())
-app.use(cors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  }))
+const corsOptions = {
+    origin: ["http://localhost:3000", "https://spotfood-frontend.vercel.app/"],
+    allowedHeaders: "Access-Control-Allow-Origin"
+}
+app.options("*",cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use(routes)
