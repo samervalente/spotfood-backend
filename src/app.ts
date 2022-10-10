aa271f8d57fa1495e8e4351d69ffef4186ef8770
samervalente <samervalente@gmail.com>
2022-10-10 11:25:52 -0300

    fix: add cors configure options

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,13 @@ import routes from "./routes/index"
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware"
 
 const app = express()
-app.use(cors())
+app.options('*', cors())
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }))
 app.use(express.json())
 
 app.use(routes)
